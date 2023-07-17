6cbc51479dd86409929e9c63b36a736bb8e6b54c
SHENG Simin <siminsheng12123@gmail.com>
2023-07-17 10:05:16 +0800

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -2,16 +2,12 @@
  *  @author YOUR NAME HERE
  */
 public class Collatz {
-
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
+        if (n % 2 == 0) {
+            return n/2;
         }
+        else return 3 * n + 1;
     }
 
     public static void main(String[] args) {
@@ -21,7 +17,6 @@ public class Collatz {
             n = nextNumber(n);
             System.out.print(n + " ");
         }
-        System.out.println();
     }
 }
 
